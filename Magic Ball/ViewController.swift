713f5efc36027d54ae127cb56f017f1f4ca8d211
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2022-02-09 00:47:06 +0530

    Initial Commit

---
+++ Magic Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic Ball
+//
+//  Created by Debojeet Das on 09/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
